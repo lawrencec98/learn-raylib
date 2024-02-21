637c7584f3d0a72d5f59c7144001942a2a53fd89
Lawrence Catapang <lawrencemcatapang@gmail.com>
2024-02-21 20:36:45 +0000

    progress on snake game

--- raylib_template/main.cpp
+++ raylib_template/main.cpp
@@ -3,167 +3,96 @@
 
 using namespace std;
 
-Color Green = Color{38, 185, 154, 255};
-Color Dark_Green = Color{20, 160, 133, 255};
-Color Light_Green = Color{129, 204, 184, 255};
-Color Yellow = Color{243, 213, 91, 255};
 
-int player_score = 0;
-int cpu_score = 0;
-
-class Ball {
+class Snake {
 public:
-    float x,y;
-    int speed_x,speed_y;
-    int radius;
+    int x, y;
+    int speed_x, speed_y;
+    int length;
 
     void Draw() {
-        DrawCircle(x, y, radius, Yellow);
+        DrawCircle(x, y, 10, RED);
     }
 
     void Update() {
+        // Update snake location
         x += speed_x;
         y += speed_y;
 
-        if (y + radius >= GetScreenHeight() || y - radius <= 0) {
-            speed_y *= -1;
+        // Update movement according to arrow key
+        if (IsKeyDown(KEY_UP)) {
+            speed_y = -5;
+            speed_x = 0;
         }
-        if (x + radius >= GetScreenWidth()) { // CPU wins
-            cpu_score++;
-            ResetBall();
+        if (IsKeyDown(KEY_DOWN)) {
+            speed_y = 5;
+            speed_x = 0;
         }
-        
-        if (x - radius <= 0) { // Player wins
-            player_score++;
-            ResetBall();
+        if (IsKeyDown(KEY_LEFT)) {
+            speed_x = -5;
+            speed_y = 0;
         }
-
-    }
-
-    void ResetBall() {
-        x = GetScreenWidth() / 2;
-        y = GetScreenWidth() / 2;
-
-        int speed_choices[2] = {-1,1};
-        speed_x *= speed_choices[GetRandomValue(0,1)];
-        speed_y *= speed_choices[GetRandomValue(0,1)];
+        if (IsKeyDown(KEY_RIGHT)) {
+            speed_x = 5;
+            speed_y = 0;
+        }
     }
 };
 
 
-class Paddle {
-protected:
-    void LimitMovement() {
-        if (y <= 0) {
-            y = 0;
-        }
-        if (y + height >= GetScreenHeight()) {
-            y = GetScreenHeight() - height;
-        }        
-    }
-
+class Food {
 public:
-    float x,y;
-    float width, height;
-    int speed_y;
+    int x, y;
 
     void Draw() {
-        DrawRectangleRounded(Rectangle{x,y,width,height}, 0.8, 0, WHITE);
-
-    }
-
-    void Update() {
-        if (IsKeyDown(KEY_UP)) {
-            y = y - speed_y;
-        }
-        else if (IsKeyDown(KEY_DOWN)) {
-            y = y + speed_y;
-        }
-
-        LimitMovement();
+        DrawCircleLines(
+            GetRandomValue(0+5,GetScreenWidth()-5),
+            GetRandomValue(0+5,GetScreenHeight()-5),
+            5,GREEN);
     }
 };
 
 
-class CpuPaddle : public Paddle {
-public:
-    void Update(int ball_y) {
-        if (y + height/2 > ball_y) {
-            y = y - speed_y;
-        }
-        if (y + height/2 < ball_y) {
-            y = y + speed_y;
-        }
-        
-        LimitMovement();
-    }
-};
-
-
-Ball ball;
-Paddle player;
-CpuPaddle cpu;
+Snake snake;
+Food food;
 
 
 int main () {
 
-    cout << "Starting the game" << '\n';
+    cout << "Starting Snake Game.\n";
     const int screen_width = 1280;
     const int screen_height = 800;
-    InitWindow(screen_width, screen_height, "My Pong Game!");
+    InitWindow(screen_width,screen_height, "My Snake game");
     SetTargetFPS(60);
 
-    ball.radius = 20;
-    ball.x = screen_width/2;
-    ball.y = screen_height/2;
-    ball.speed_x = 7;
-    ball.speed_y = 7;
+    // Initialize snake
+    snake.x = screen_width / 2;
+    snake.y = screen_height / 2;
+    snake.speed_x = 0;
+    snake.speed_y = 0;
+    snake.length = 1;
 
-    player.width = 25;
-    player.height = 120;
-    player.x = screen_width - player.width -10;
-    player.y = screen_height/2 - player.height/2;
-    player.speed_y = 6;
-
-    cpu.height = 120;
-    cpu.width = 25;
-    cpu.x = 10;
-    cpu.y = screen_height/2 - cpu.height/2;
-    cpu.speed_y = 6;
-
+    food.Draw();
 
     while(!WindowShouldClose()) {
-
+        
         BeginDrawing();
 
-        // Updating
-        ball.Update();
-        player.Update();
-        cpu.Update(ball.y);
+        //ClearBackground(BLACK);
 
-        // Checking for collisions
-        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height})) {
-            ball.speed_x *= -1;
-        }
-        if(CheckCollisionCircleRec(Vector2{ball.x,ball.y}, ball.radius, Rectangle{cpu.x,cpu.y,cpu.width,cpu.height})) {
-            ball.speed_x *= -1;
-        }
-
-        // Drawing
-        ClearBackground(Dark_Green);
-        DrawRectangle(screen_width/2, 0, screen_width/2, screen_height, Green);
-        DrawCircle(screen_width/2, screen_height/2, 150, Light_Green);
-        DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
-        ball.Draw();
-        cpu.Draw();
-        player.Draw();
-        DrawText(TextFormat("%i",cpu_score), screen_width/4 - 20, 20, 80, WHITE);
-        DrawText(TextFormat("%i",player_score), 3 * screen_width/4 - 20, 20, 80, WHITE);
+        // Draw Snake
+        snake.Draw();
+        snake.Update();
 
         EndDrawing();
 
     }
 
-    CloseWindow();
     return 0;
 }
+
+// to-do
+// need to add collision detection snake-snake
+// need to add collision detection snake-food
+// need to add snake boundary interaction
+// need to add snake growth
